55d18aa276a7ab8f5a8eeaa63126b747538b0e2f
ap0calypse13 <155027431+ap0calypse13@users.noreply.github.com>
2024-07-04 01:12:09 +0330

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Navbar from "./components/Navbar"
+import { Container } from "react-bootstrap"
+function App() {
+  return(
+    
+    <Container>
+      <Navbar></Navbar>
+    </Container>
+ 
+
+
+  )
+}
+ export default App
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import{Navbar as NavbarBs , Button} from 'react-bootstrap'
+
+
+
+function Navbar() {
+  return(
+ <NavbarBs>
+    <NavbarBs.Collapse>
+        <Button>Cart</Button>
+    </NavbarBs.Collapse>
+ </NavbarBs> 
+     
+ )
+
+}
+export default Navbar
